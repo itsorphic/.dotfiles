0ec1dc1f5cd5346dc64df7018c63485d1a149c22
itsorphic <orphicxbt@proton.me>
2025-05-04 08:30:19 +0100

    vim lsps

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -1,15 +1,42 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
+    formatters_by_ft = {
+        lua = { "stylua" },
+        c_cpp = { "clang-format" },
+        c = { "clang_format" },
+        cpp = { "clang_format" },
+        css = { "prettier" },
+        html = { "prettier" },
+        python = { "black", "isort" },
+    },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+    formatters = {
+        ["clang-format"] = {
+            prepend_args = {
+                "-style={ \
+                IndentWidth: 4, \
+                TabWidth: 4, \
+                UseTab: Never, \
+                AccessModifierOffset: 0, \
+                IndentAccessModifiers: true, \
+                PackConstructorInitializers: Never}",
+            },
+        },
+        -- python
+        black = {
+            prepend_args = {
+                "--fast",
+                "--line-length",
+                "80",
+            },
+        },
+    },
+
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
 }
 
+require("conform").setup(options)
 return options
